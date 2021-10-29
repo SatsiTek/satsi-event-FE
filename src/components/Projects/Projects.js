e5d337b8661cfc375f29d7dca071f6453fee2359
shoppinh <mactrungkien2000@gmail.com>
2021-10-29 16:36:04 +0700

    ....

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,20 +1,20 @@
-import React, { useRef } from 'react';
-import './Projects.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
-import 'swiper/swiper.min.css';
-import 'swiper/components/navigation/navigation.min.css';
-import 'swiper/components/pagination/pagination.min.css';
-import img1 from '../../asset/img-sample1.jpg';
-import img2 from '../../asset/img-sample2.jpg';
-import img3 from '../../asset/img-sample3.jpg';
-import img4 from '../../asset/img-sample4.jpg';
+import React, { useRef } from "react";
+import "./Projects.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination } from "swiper";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import img1 from "../../asset/img-sample1.jpg";
+import img2 from "../../asset/img-sample2.jpg";
+import img3 from "../../asset/img-sample3.jpg";
+import img4 from "../../asset/img-sample4.jpg";
 
 const SLIDES = [
   {
     img: img1,
-    category: 'Category / Website',
-    title: 'Entertainment',
+    category: "Category / Website",
+    title: "Entertainment",
     content: ` Lorem ipsum magicum dolor sit amet, consectetur adipiscing
   elit. Maecenas a sem ultrices neque vehicula fermentum a sit
   amet nulla.`,
@@ -23,7 +23,7 @@ const SLIDES = [
     img: img2,
     category: `
     Photo / Instagram`,
-    title: 'Last Exploration',
+    title: "Last Exploration",
     content: ` Lorem ipsum magicum dolor sit amet, consectetur adipiscing
   elit. Maecenas a sem ultrices neque vehicula fermentum a sit
   amet nulla.`,
@@ -32,17 +32,17 @@ const SLIDES = [
     img: img3,
     category: `
   Arts / Painting`,
-    title: 'Painting',
+    title: "Painting",
     content:
-      'Lorem ipsum magicum dolor sit amet, consectetur adipiscing elit. Maecenas a sem ultrices neque vehicula fermentum a sit amet nulla.',
+      "Lorem ipsum magicum dolor sit amet, consectetur adipiscing elit. Maecenas a sem ultrices neque vehicula fermentum a sit amet nulla.",
   },
   {
     img: img4,
     category: `
   Photo / Instagram`,
-    title: 'Macro Lens',
+    title: "Macro Lens",
     content:
-      'Lorem ipsum magicum dolor sit amet, consectetur adipiscing elit. Maecenas a sem ultrices neque vehicula fermentum a sit amet nulla.',
+      "Lorem ipsum magicum dolor sit amet, consectetur adipiscing elit. Maecenas a sem ultrices neque vehicula fermentum a sit amet nulla.",
   },
 ];
 SwiperCore.use([Navigation, Pagination]);
@@ -53,34 +53,34 @@ const Projects = () => {
   const swiperRef = useRef(null);
   console.log(prevRef.current, nextRef.current);
   return (
-    <div className='projectsContain'>
-      <div className='leftColumn'>
-        <div className='leftContent'>
+    <div className="projectsContain">
+      <div className="leftColumn">
+        <div className="leftContent">
           <h2>Projects</h2>
           <p>Just a few sample among our huge amount of projects.</p>
-          <button className='registerBtn'>Register</button>
+          <button className="registerBtn">Register</button>
           <ion-icon
             style={{
               fontSize: 64,
-              position: 'absolute',
+              position: "absolute",
               left: 30,
               bottom: 30,
             }}
-            name='images-outline'
+            name="images-outline"
           ></ion-icon>
         </div>
       </div>
-      <div className='rightColumn'>
-        <div className='slideRightContent'>
-          <div className='navigationBtn'>
+      <div className="rightColumn">
+        <div className="slideRightContent">
+          <div className="navigationBtn">
             <div
-              className='prevBtn'
+              className="prevBtn"
               onClick={() => swiperRef.current.swiper.slidePrev()}
             >
               prev
             </div>
             <div
-              className='nextBtn'
+              className="nextBtn"
               onClick={() => swiperRef.current.swiper.slideNext()}
             >
               next
@@ -96,9 +96,9 @@ const Projects = () => {
             {SLIDES.map((slide, index) => (
               <SwiperSlide key={index}>
                 <div>
-                  <img className='slideImage' src={slide.img} />
-                  <p className='slideCategory'>{slide.category}</p>
-                  <h2 className='slideTitle'>{slide.title} </h2>
+                  <img className="slideImage" src={slide.img} alt="" />
+                  <p className="slideCategory">{slide.category}</p>
+                  <h2 className="slideTitle">{slide.title} </h2>
                   <p>{slide.content}</p>
                 </div>
               </SwiperSlide>
--- src/components/Wrapper/index.jsx
+++ src/components/Wrapper/index.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import Test1 from '../Test1/';
-import Home from '../Home';
-import Sidebar from '../Sidebar';
-import './Wrapper.css';
-import _ from 'lodash';
-import AboutUs from '../AboutUs';
-import Services from '../Services';
-import Projects from '../Projects/Projects';
+import React from "react";
+import Home from "../Home";
+import Sidebar from "../Sidebar";
+import "./Wrapper.css";
+import _ from "lodash";
+import AboutUs from "../AboutUs";
+import Services from "../Services";
+import Projects from "../Projects/Projects";
 const eventArray = [
-  { href: 'hihi', icon: <ion-icon name='logo-angular'></ion-icon> },
-  { href: 'hiha', icon: <ion-icon name='logo-css3'></ion-icon> },
-  { href: 'hiho', icon: <ion-icon name='logo-octocat'></ion-icon> },
-  { href: 'hihafaf', icon: <ion-icon name='logo-github'></ion-icon> },
+  { href: "hihi", icon: <ion-icon name="logo-angular"></ion-icon> },
+  { href: "hiha", icon: <ion-icon name="logo-css3"></ion-icon> },
+  { href: "hiho", icon: <ion-icon name="logo-octocat"></ion-icon> },
+  { href: "hihafaf", icon: <ion-icon name="logo-github"></ion-icon> },
   // { href: 'concacon', icon: <ion-icon name='logo-javascript'></ion-icon> },
 ];
 
@@ -50,42 +49,42 @@ const Wrapper = () => {
     [updatePosition, position]
   );
   React.useEffect(() => {
-    window.addEventListener('wheel', handleNavigation);
+    window.addEventListener("wheel", handleNavigation);
     return () => {
-      window.removeEventListener('wheel', handleNavigation);
+      window.removeEventListener("wheel", handleNavigation);
     };
   }, [handleNavigation]);
   React.useEffect(() => {
     wrapperEls.current[position].scrollIntoView({
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }, [position]);
   return (
     <React.Fragment>
-      <div className='wrapper'>
-        <div className={position === 0 ? 'homeContain active' : 'homeContain'}>
+      <div className="wrapper">
+        <div className={position === 0 ? "homeContain active" : "homeContain"}>
           <Home
             ref={(el) => (wrapperEls.current[0] = el)}
-            id={'home'}
-            timeTillDate='2021/10/31 14:27:06'
+            id={"home"}
+            timeTillDate="2021/10/31 14:27:06"
           />
         </div>
 
         <div
-          className={position === 1 ? 'aboutUs active' : 'aboutUs'}
+          className={position === 1 ? "aboutUs active" : "aboutUs"}
           ref={(el) => (wrapperEls.current[1] = el)}
         >
           <AboutUs />
         </div>
 
         <div
-          className={position === 2 ? 'services active' : 'services'}
+          className={position === 2 ? "services active" : "services"}
           ref={(el) => (wrapperEls.current[2] = el)}
         >
           <Services />
         </div>
         <div
-          className={position === 3 ? 'projects active' : 'projects'}
+          className={position === 3 ? "projects active" : "projects"}
           ref={(el) => (wrapperEls.current[3] = el)}
         >
           <Projects />
